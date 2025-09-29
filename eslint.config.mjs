0688a78ed5fa8baa76e500a34b90ba20a5c04b9c
Cole Reed <ichabodcole@gmail.com>
2025-09-29 13:46:34 -0600

    chore: update turbo to v2, add turbo eslint and config updates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
 import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
 import eslintPluginPrettierRecommended from 'eslint-config-prettier';
+import turboPlugin from 'eslint-config-turbo/flat';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
+  turboPlugin,
   {
     files: ['**/*.ts'],
     languageOptions: {
